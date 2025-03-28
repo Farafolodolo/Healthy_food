e9fded052be2588fc153086459666561c4584cbe
David Portillo <a1123150055@utch.edu.mx>
2025-03-28 13:43:42 -0600

    Add the route of the rain forest api

---
+++ routes/main.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, render_template, request, jsonify
+import requests
+from models import get_cursor
+#A blueprint is a way to declarate the endpoints
+main_bp = Blueprint("main", __name__)
+
+RAIN_FOREST_KEY = ""
+
+@main_bp.route('/')
+def index():
+    return render_template('main_page.html')
+
+@main_bp.route('/search_products_amazon')
+def search_products_amazon():
+    search = request.args.get('q',default='')
+    domain = 'amazon.com'
+    page = request.args.get('page', default=1,type=int)
+
+    if search:
+        params_search = {
+            "api_key": RAIN_FOREST_KEY,
+            "type": "search",
+            "amazon_domain": domain,
+            "search_term": search,
+            "page": page
+        }
+    try:
+        response = requests.get(response = requests.get('https://api.rainforestapi.com/request', params=params_search))
+        response.raise_for_status()
+
+        data = response.json()
+
+        results = []
+
+        if 'search_results' in data:
+            for product in data['search_results']:
+                results.append({
+                    'titulo': product.get('title'),
+                    'precio': product.get('price', {}).get('value'),
+                    'moneda': product.get('price', {}).get('currency'),
+                    'url': product.get('link'),
+                    'imagen': product.get('image')
+                })
+
+        return jsonify({
+            'status': 'success',
+            'results': results,
+            'total_resultados': len(results)
+        })
+    except requests.exceptions.RequestException as e:
+        return jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500
+    
